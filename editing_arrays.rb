1c24b398fb949e25dd3b7c86b654d3acf52585d9
spethani <sarahpethani@gmail.com>
2018-08-08 13:27:37 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -4,11 +4,12 @@ rainbow_colors = [
   "light_cyan"
 ]
 
-def change_rainbow_colors (rainbow_colors)
+def change_rainbow_colors 
   # Write a solution that corrects the three colors in rainbow_colors, then returns the corrected array
-  rainbow_colors[0]="red"
-  rainbow_colors[1]="light_red"
-  rainbow_colors[2]="light_yellow"
+  #rainbow_colors[0]="red"
+  #rainbow_colors[1]="light_red"
+  #rainbow_colors[2]="light_yellow"
+  rainbow_colors=["red", "light_red", "light_yellow"]
   puts rainbow_colors
 end
 
